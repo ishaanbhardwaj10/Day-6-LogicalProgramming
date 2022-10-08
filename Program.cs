c6a2d25353b1240d2f1e97479ca997549ce25fd8
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-08 17:20:46 +0530

    added PP-2 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Welcome to Logical Programs in C#");
             Console.WriteLine("Program #1: Fibonacci Series");
+            Console.WriteLine("Program #2: Perfect Numbers");
 
             Console.Write("Please select a program to run from options above: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -14,6 +15,9 @@
             {
                 case 1:
                     FibonacciSeries.PrintSeries();
+                    break;
+                case 2:
+                    PerfectNumbers.PrintPerfectNumbers();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid number from given options");
